c6b739e70bb52b6c520b81b1b61f736feb296ed7
Reid Harry <48355869+Reido50@users.noreply.github.com>
2022-10-25 18:52:11 -0400

    Finished HW4 Q1

--- Homework 2/Q1/main.py
+++ Homework 2/Q1/main.py
@@ -11,15 +11,12 @@ for l in range(num_tests):
 # Traverse nodes to find first good starting node
 current_start = 0
 current_fuel = 0
-current_run = 1
 for curr in range(len(nodes)):
     current_fuel += nodes[curr][0]
     current_fuel -= nodes[curr][1]
     if current_fuel >= 0:
-        current_run += 1
         if curr == len(nodes) - 1:
             print(current_start)
     else:
         current_start = curr + 1
         current_fuel = 0
-        current_run = 1
