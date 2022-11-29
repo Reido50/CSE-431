1c3fed68a5a5a11419eb13e395121b3bec289ec9
Reid Harry <48355869+Reido50@users.noreply.github.com>
2022-11-29 13:27:56 -0500

    Finished Q1

--- Homework 6/Q1/main.py
+++ Homework 6/Q1/main.py
@@ -10,41 +10,41 @@ for i in range(num_candidates):
     skills = set(input().split())
     candidates.append((num_skills, skills))
 
-best = 9999
+best = 999999
 
 # req_skills is the set of skills that are requires to be covered by our candidate choices
 # num_req_skills is the number of requires skills
 # candidates is a list of tuples that represent a candidate (num_skills, skills set)
 # num_candidates is the number of available candidates to check
 
-def covers_reqs(s):
-    return s.difference(req_skills) == set()
-
-def is_valid(included):
-    weight = 0
-    for el in included:
-        weight += item_weights[el]
-
-    return weight <= weight_limit
-
-
 def solve_knapsack(included, remaining):
     global best
 
+    # If we already have too many candidates, give up
+    if included[0] >= best:
+        return 999999
+
+    # If we have enough skills, check if it's the best
     if len(included[1]) >= num_req_skills:
-        if covers_reqs(included[1]):
+        if included[1].difference(req_skills) == set():
             result = included[0]
             if result < best:
                 best = result
             return result
 
+    # If we reached the end, give up
     if len(remaining) == 0:
-        return 9999
+        return 999999
 
+    # If our next recurs are doomed, don't even do them
+    if (included[0] + 1) >= best:
+        return 999999
+
+    # Grab another candidate
     curr_item = remaining.pop()
-
     new_included = [included[0] + 1, included[1].union(curr_item[1])]
 
+    # Try with or without youuuuuuuuuu, WITH YOUR WITHOUT YOUUUUUU OOOOOOOHHHH. I CANT LIIIIIIVE WITH OR WITHOUT YOUUUUUUUUU
     incl_value = solve_knapsack(new_included[:], remaining[:])
     excl_value = solve_knapsack(included[:], remaining[:])
 
